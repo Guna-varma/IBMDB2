e11bb28178b316ad3a37c6bac07f540639efda6d
Guna-Varma-23 <meda.varma@cubastion.com>
2024-12-10 12:11:39 +0530

    Project Setup

---
+++ src/main/java/com/projectdb2/ibmdb2/Ibmdb2Application.java
@@ -0,0 +1,13 @@
+package com.projectdb2.ibmdb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ibmdb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ibmdb2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/projectdb2/ibmdb2/Ibmdb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectdb2.ibmdb2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ibmdb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
